169e52a771ba80e65a5ad3a1e83ffe20de971f88
koromerzhin <308012+koromerzhin@users.noreply.github.com>
2023-02-02 11:16:23 +0100

    wip(ci/cd): add cypress

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,6 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://wordpress.traefik.me')
+    cy.visit('https://wordpress.traefik.me');
+    cy.screenshot('first-page');
   })
 })
